5aa3156e68f36a79dd7867d48654e0aa0623c245
priyankapeelu <priyankapeelu@icloud.com>
2022-05-10 14:17:23 +0530

    terraform variablescd tt

--- cluster.tf
+++ cluster.tf
@@ -25,21 +25,21 @@ resource "aws_security_group" "allow_rabbitmq" {
 }
 
 resource "aws_mq_broker" "rabbitmq" {
-  broker_name        = "roboshop-${var.ENV}"
+  broker_name = "roboshop-${var.ENV}"
+
+  //  configuration {
+  //    id       = aws_mq_configuration.config-main.id
+  //    revision = aws_mq_configuration.config-main.latest_revision
+  //  }
+
   engine_type        = "RabbitMQ"
   engine_version     = var.RABBITMQ_ENGINE_VERSION
   host_instance_type = var.RABBITMQ_INSTANCE_TYPE
   security_groups    = [aws_security_group.allow_rabbitmq.id]
   subnet_ids         = [data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS[0]]
-}
-
 
   user {
     username = "roboshop"
     password = "RoboShop1234"
   }
-}
-
-output "rabbitmq" {
-  value = aws_mq_broker.rabbitmq
 }
