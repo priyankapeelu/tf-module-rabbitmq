f3438d9f8951b436c8a525ff89664a6b0c72594c
priyankapeelu <priyankapeelu@icloud.com>
2022-05-10 14:05:17 +0530

    terraform variablescd tt

--- cluster.tf
+++ cluster.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "allow_rabbitmq" {
 
   ingress {
     description = "TLS from VPC"
-    from_port   = var.RDS_MYSQL_PORT
-    to_port     = var.RDS_MYSQL_PORT
+    from_port   = var.RABBITMQ_PORT
+    to_port     = var.RABBITMQ_PORT
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
