5fbf16dcf78205085ce4af5a8762279784512382
priyankapeelu <priyankapeelu@icloud.com>
2022-06-06 19:56:28 +0530

    terraform variablescd tt

--- data.tf
+++ data.tf
@@ -15,6 +15,3 @@ data "aws_secretsmanager_secret" "secrets" {
 data "aws_secretsmanager_secret_version" "secrets" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
-
-mongo --ssl --sslCAFile /home/centos/rds-combined-ca-bundle.pem --host roboshop-dev.cluster-cunmblsnrv4v.us-east-1.docdb.amazonaws.com --username admin1 --password roboshop1 < catalogue.js
-mongo --ssl --sslCAFile /home/centos/rds-combined-ca-bundle.pem --host roboshop-dev.cluster-cunmblsnrv4v.us-east-1.docdb.amazonaws.com --username admin1 --password roboshop1  < users.js
